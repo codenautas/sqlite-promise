e205a9efb851c6ab390f0e63ff5fddbaf55fee28
Emilio <emilioplatzer@gmail.com>
2015-09-01 12:52:26 -0300

    agrego la creación de la base y pongo testUntil

--- test/test-sql-promise.js
+++ test/test-sql-promise.js
@@ -1,7 +1,31 @@
 "use strict";
 
 var tester=require('sql-promise').tester;
+var fs=require('fs-promise');
+
+var sqlite3 = require('sqlite3');
 
 var sqlite = require('..');
 
-tester.test(sqlite, {connOpts: ':memory:', badConnOpts: 'inexis_file.db'});
+var dbFile='local-database.db';
+
+function prepareFile(){
+    return fs.unlink(dbFile).catch(function(err){
+        if(err.code!=='ENOENT'){
+            throw err;
+        }
+    }).then(function(){
+        console.log('creando ',dbFile);
+        var db = new sqlite3.Database(dbFile); // uso un archivo real para poder mirarlo
+        console.log(db);
+        db.run("CREATE TABLE lorem (info TEXT)");
+        return null; //ok
+    });
+}
+
+tester.test(sqlite, {
+    connOpts: dbFile, 
+    badConnOpts: 'inexis_file.db', 
+    prepare:prepareFile,
+    testUntil:'connect'
+});
