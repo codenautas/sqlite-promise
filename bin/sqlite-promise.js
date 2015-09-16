e997f71228514edbd843214820e1f25af988ed5e
Diego F.(i7) <diegoefe@gmail.com>
2015-09-16 10:09:27 -0300

    Adapto interfaz a la de pg-promise-plus

--- bin/sqlite-promise.js
+++ bin/sqlite-promise.js
@@ -6,9 +6,80 @@
 var sqlitePromise = {};
 
 var Promises = require('best-promise');
-var sqlPromise = require('sql-promise');
 var sqlite3 = require('sqlite3').verbose();
 
+sqlitePromise.Motor = function SqliteMotor(){
+}
+
+sqlitePromise.sqlite3 = sqlite3;
+
+function applyToConstructor(constructor, argArray) {
+    var args = [null].concat(argArray);
+    var factoryFunction = constructor.bind.apply(constructor, args);
+    return new factoryFunction();
+}
+
+sqlitePromise.Motor.connect = function connect(connectParameters){
+    return Promises.make(function(resolve, reject){
+        var mode = 0;
+        if(connectParameters.create) { mode |= sqlite3.OPEN_CREATE; }
+        if(connectParameters.readWrite || !mode) { mode |= sqlite3.OPEN_READWRITE; }
+        var con = new sqlite3.Database(connectParameters.file, mode, function(err) {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(con);
+            }
+        });
+    });
+};
+
+sqlitePromise.Motor.done = function connect(internal){
+    return Promises.start(function(){
+        return internal.done();
+    });
+};
+
+sqlitePromise.Motor.prepare = function prepare(internal, sqlSentence){
+    return Promises.make(function(resolve, reject) {
+        var stmt = internal.prepare(sqlSentence, function(err) {
+            if(err) {
+                reject(err);
+            } else {
+                resolve({
+                   conn:internal,
+                   stmt:stmt,
+                   sql:sqlSentence
+                });
+            }
+        });
+    });
+};
+
+sqlitePromise.Motor.query = function query(internal, data){
+    return Promises.start(function() {
+       return {
+            conn:internal.conn,
+            stmt:internal.stmt,
+            sql:internal.sql,
+            data:data
+        };
+    });
+};
+
+sqlitePromise.Motor.fetchAll = function fetchAll(internal){
+    return Promises.make(function(resolve, reject){
+        internal.stmt.all(function(err, result){
+            if(err){
+                reject(err);
+            }else{
+                resolve(result);
+            }
+        });
+    });
+};
+
+/*
 function SqliteConnection(params) {
     this.db = new sqlite3.Database(params, sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE, function(err) {
         console.log("*****", err);
@@ -102,7 +173,7 @@ sqlitePromise.Motor = function SQLiteMotor() {
         });
     };
 };
-
+*/
 
 // sqlitePromise = Object.create(sqlPromise);
 
--- test/test.js
+++ test/test.js
@@ -1,34 +1,91 @@
 "use strict";
 
-var sp = require('..');
+var Promises = require('best-promise');
+var fs=require('fs-promise');
+var tester=require('sql-promise-tester');
 
-var motor = new sp.Motor;
+var MotorSqlite = require('..').Motor;
 
-// motor.connect("local-database.db").then(function(con) {
-    // console.log("con", con);
-    // return motor.exec(con, "CREATE table t1(id integer, name varchar);");
-// }).then(function(r) {
-    // console.log("res", r);
-    // motor.close();
-// }).catch(function(err) {
-    // console.log("err", err);
-// });
+var defaultConnOpts={
+    file:'test.db3',
+    //file:':memory:',
+    //create: true,
+    readWrite: true
+};
 
-motor.connect("local-database.db").then(function(con) {
-    console.log("con", con);
-    return motor.prepare(con, "CREATE table t1(id integer, name varchar);");
+var motor = MotorSqlite;
+
+function prepareFile(){
+    var dbFile = defaultConnOpts.file;
+    return fs.unlink(dbFile).catch(function(err){
+        if(err.code!=='ENOENT'){
+            throw err;
+        }
+    }).then(function(){
+        console.log('creando ',dbFile);
+        var sqlite3 = require('..').sqlite3;
+        var db = new sqlite3.Database(dbFile, sqlite3.OPEN_CREATE |sqlite3.OPEN_READWRITE); // uso un archivo real para poder mirarlo
+        console.log(db);
+        db.serialize(function() {
+            db.run("CREATE TABLE lorem (info TEXT)");
+            var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+            for (var i = 0; i < 10; i++) {
+                stmt.run("Ipsum " + i);
+            }
+            stmt.finalize();
+        });
+        db.close();
+        console.log("prepareFile() done");
+        return null; //ok
+    }).catch(function(err) {
+        console.log("ERROR", err.stack);
+        throw err;
+    });
+}
+
+prepareFile().then(function() {
+    console.log("listo");
+}).then(function() {
+    return motor.connect(defaultConnOpts);
+}).then(function(con) {
+    return motor.prepare(con, "SELECT * FROM sqlite_master WHERE type='table'");
+    //return motor.prepare(con, "SELECT info FROM lorem WHERE 1");
 }).then(function(prepared) {
-    console.log("prepared", prepared);
+    // console.log("prepared", prepared);
     return motor.query(prepared, "");
 }).then(function(query) {
-    console.log("query", query);
+    // console.log("query", query);
     return motor.fetchAll(query);
 }).then(function(result) {
     console.log("result", result); 
 }).catch(function(err) {
     console.log("err", err.stack);
-});
+});    
 
+/*
+describe('internal test', function() {
+    before(function(done) {
+       prepareFile().then(done, done); 
+    });
+    it('must perform a simple select', function(done) {
+        done();
+        // motor.connect(defaultConnOpts).then(function(con) {
+            // console.log("con", con);
+            // return motor.prepare(con, "SELECT * FROM lorem;");
+        // }).then(function(prepared) {
+            // console.log("prepared", prepared);
+            // return motor.query(prepared, "");
+        // }).then(function(query) {
+            // console.log("query", query);
+            // return motor.fetchAll(query);
+        // }).then(function(result) {
+            // console.log("result", result); 
+        // }).catch(function(err) {
+            // console.log("err", err.stack);
+        // });        
+    });
+});
+*/
 
 // var tester=require('sql-promise-tester');
 // var fs=require('fs-promise');
